c9f22ffaabb0694c11b2f54c4e8c024459335dcc
KYHH <kyhh93@naver.com>
2023-11-15 23:07:24 +0900

    fix: 테스트 에러 예외처리 while break 시도

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -6,20 +6,22 @@ const InputView = {
 
     async readDate() {
         Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
+        let inputDate;
         while (true) {
             try {
                 const input = await Console.readLineAsync(`12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n`);
-                const inputDate = parseInt(input, 10);
+                inputDate = parseInt(input, 10);
                 Validator.dateInput(inputDate);
-
-                return inputDate;
+                break;
             } catch (error) {
                 Console.print(error.message);
             }
         }
+        return inputDate;
     },
 
     async orderMenu() {
+        let ordersInput = null;
         while (true) {
             try {
                 const input = await Console.readLineAsync(`주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n`);
@@ -30,13 +32,15 @@ const InputView = {
                 orders.forEach(order => Validator.validateMenuItem(order.name));
                 orders.forEach(order => Validator.validateOrderCount(order.count));
 
-                return input;
+                ordersInput = input;
+                break;
             } catch (error) {
                 Console.print(error.message);
             }
         }
+        return ordersInput;
     }
 
 }
 
-export default InputView;
+export default InputView; 
