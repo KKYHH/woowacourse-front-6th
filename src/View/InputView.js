697cbb6a7a8a24db54e874d2e07244a955c5dcdd
KYHH <kyhh93@naver.com>
2023-11-15 23:27:49 +0900

    fix 테스트 에러 예외처리 Console.close 시도

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -10,9 +10,9 @@ const InputView = {
         while (true) {
             try {
                 const input = await Console.readLineAsync(`12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n`);
+                Console.close;
                 inputDate = parseInt(input, 10);
                 Validator.dateInput(inputDate);
-                Console.close;
                 break;
             } catch (error) {
                 Console.print(error.message);
@@ -27,6 +27,7 @@ const InputView = {
             try {
 
                 const input = await Console.readLineAsync(`주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n`);
+                Console.close;
                 const orders = ParseOrders.parse(input);
                 Validator.menuInput(input);
                 Validator.validateDuplicateMenu(orders);
@@ -35,7 +36,6 @@ const InputView = {
                 orders.forEach(order => Validator.validateOrderCount(order.count));
 
                 ordersInput = input;
-                Console.close;
                 break;
             } catch (error) {
                 Console.print(error.message);
